d8a0fc1805e4bf68adb7e4be4fd16c9b0d08e145
tomashasmat <130392093+tomashasmat@users.noreply.github.com>
2025-10-21 22:11:28 -0300

    Update main.js

--- tp/main.js
+++ tp/main.js
@@ -1,3 +1,33 @@
+function cargarLista(e){
+    const materiaId = e.target.value;
+    fetch('http://localhost:3000/api/alumnos/')
+        .then(res => res.json())
+        .then(data => {
+            let tbody = document.querySelector('tbody');
+            tbody.innerHTML = '';
+            const btns = ['P', 'A', 'T', 'RA'];
+            for(let alumno of data){
+                let tr = document.createElement('tr');
+                let aid = document.createElement('td');
+                let nombre = document.createElement('td');
+                let apellido = document.createElement('td');
+                aid.textContent = id;
+                nombre.textContent = nombres;
+                apellido.textContent = apellidos;
+                tr.append(aid, nombre, apellido);
+                let td = document.querySelector('td');
+                for(let text of btns){
+                    let button = document.createElement('button');
+                    button.textContent = text;
+                    button.onClick = handleClick;
+                    td.append(button);
+                }
+                tr.append('td');
+                tbody.append('tr');
+            }
+        });
+}
+
 function cargarCursos(){
     fetch('http://localhost:3000/api/cursos/')
         .then(res => res.json)
@@ -32,9 +62,28 @@ function cargarMaterias(e){
 }
 
 function handleClick(event){
+    let row = event.target.parentElement.parentElement;
+    let idAlumno = row.children[0].textContent;
+    let select = document.querySelector('#materia');
+    let idMateria = select.value;
     let datos = {
         tipo: event.target.textContent,
-        alumno: 2,
-        materia: 1
+        alumno: idAlumno,
+        materia: idMateria
     };
+
+    const options = {
+        method = 'POST',
+        body: JSON.stringify(datos),
+        headers: {'Content-Type': 'application/json'}
+    }
+
+    const url = ('https://localhost:3000/api/asistencias');
+    fetch(url, options)
+        .then(res => res.json())
+        .then(data {
+
+        });
+    .catch(err => alert(err.stack));
+
 }
