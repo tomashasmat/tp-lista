af23563071895a22bf37d66f4e89d936c89a273d
Foxygamer72 <agustinurielalbinacuello@gmail.com>
2025-10-21 21:18:32 -0300

    Update union.js

--- tp/union.js
+++ tp/union.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'tp',
+  database: 'Lista',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
