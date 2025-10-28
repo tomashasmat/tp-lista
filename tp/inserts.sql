38a7f1412ee1d2614ec7a0c86a056b93dcf1ae0e
tomashasmat <130392093+tomashasmat@users.noreply.github.com>
2025-10-28 20:57:56 -0300

    Add files via upload

---
+++ tp/inserts.sql
@@ -0,0 +1,8 @@
+INSERT INTO alumnos VALUES
+    (NULL, 'Juan', 'Garcia', '47275152', '4to:1ra'),
+    (NULL, 'Federico', 'Chavez', '46812523', '5to:2da'),
+    (NULL, 'Sofia', 'Gomez', '48345756', '4to:4ta'),
+    (NULL, 'Martin', 'Perez', '47234756', '5to:2da'),
+    (NULL, 'Celeste', 'Diaz', '47235231', '5to:2da'),
+    (NULL, 'Esteban', 'Quito', '48234566', '4to:4ta'),
+    (NULL, 'Lucas', 'Molina', '')
